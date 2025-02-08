772be276f66c320ba4c81c5e73a836f4206241aa
Daniel Felipe Varela Giraldo <daniel_felipe94@hotmail.com>
2025-02-08 15:11:46 -0500

    Se inicia configuracion de rutas, error en http main-movies

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { MainMoviesComponent } from "./main-movies/main-movies.component";
 import { NavComponent } from "./nav/nav.component";
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainMoviesComponent, NavComponent, CommonModule],
+  imports: [RouterOutlet, MainMoviesComponent, NavComponent, CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainMoviesComponent } from './main-movies/main-movies.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: MainMoviesComponent},
+    {path: 'add-movie', component: AddMovieComponent}
+];
