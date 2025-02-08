3032da47d2ed0f2bd6fcf284ac26245b77bb86c3
Daniel Felipe Varela Giraldo <daniel_felipe94@hotmail.com>
2025-02-08 14:52:49 -0500

    se sirven archivos de forma estatica desde el servidor para poder mostrar las imagenes

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 dotenv.config({ path: './env/.env' });
@@ -11,26 +12,27 @@ var router = require('./src/routes/router');
 
 
 // PUERTO DEL SERVIDOR
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT;
 
 // MIDDLEWARES
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
 // HABILITAR SERVIDOR
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo por el puerto: ${PORT}`);
-    //LLAMADA A FUNCION PARA CONECTAR A BASE DE DATOS.
-    db_export.dbConnection();
+  console.log(`Servidor corriendo por el puerto: ${PORT}`);
+  //LLAMADA A FUNCION PARA CONECTAR A BASE DE DATOS.
+  db_export.dbConnection();
 });
 
-//app.use(cors()); 
 
 app.use(cors({
-    origin: 'http://localhost:4200', 
-    methods: 'GET,POST,PATCH,DELETE',
-    allowedHeaders: 'Content-Type,Authorization'
-  }));
-  
+  origin: 'http://localhost:4200',
+  methods: 'GET,POST,PATCH,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
+
+app.use('/files', express.static(path.join(__dirname, 'src/public/files')));
 
 app.use('/', router);
